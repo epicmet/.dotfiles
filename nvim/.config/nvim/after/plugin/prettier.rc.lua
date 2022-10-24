50d433e4ae7e89ccebf1bf061ed792dfa16a365c
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-10-24 12:26:19 +0330

    Brought back autocmd for prettier on save in cases where null-ls just does not work

--- nvim/.config/nvim/after/plugin/prettier.rc.lua
+++ nvim/.config/nvim/after/plugin/prettier.rc.lua
@@ -18,11 +18,32 @@ prettier.setup {
 vim.keymap.set('n', '<leader>f', '<cmd>:Prettier<CR>')
 
 -- autocmd for auto format
-vim.g.auto_prettier = true
+vim.g.manual_prettier_autoformat_onsave = false
 
 vim.keymap.set('n', '<leader>pe',
-  '<cmd>lua vim.g.auto_prettier = true<CR><cmd>lua print("auto format with prettier `enabled`")<CR>'
+  '<cmd>lua vim.g.manual_prettier_autoformat_onsave = true<CR><cmd>lua print("manual auto-format with prettier is `enabled`")<CR>'
 )
 vim.keymap.set('n', '<leader>pd',
-  '<cmd>lua vim.g.auto_prettier = false<CR><cmd>lua print("auto format with prettier `disabled`")<CR>'
+  '<cmd>lua vim.g.manual_prettier_autoformat_onsave = false<CR><cmd>lua print("manual auto-format with prettier is `disabled`")<CR>'
 )
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup('manual-prettier-autoformat', { clear = true }),
+  pattern = {
+    "*.ts",
+    "*.tsx",
+    "*.js",
+    "*.cjs",
+    "*.jsx",
+    "*.html",
+    "*.json",
+    "*.md",
+    "*.css",
+    "*.scss",
+    "*.yaml"
+  },
+  callback = function()
+    if vim.g.manual_prettier_autoformat_onsave then
+      vim.api.nvim_command("Prettier")
+    end
+  end
+})
