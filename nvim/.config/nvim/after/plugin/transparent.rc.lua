7622b70b3faa532c623e3fe5c7768bf43c277750
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-10-28 17:39:24 +0330

    Disabled transparent background on macOS

--- nvim/.config/nvim/after/plugin/transparent.rc.lua
+++ nvim/.config/nvim/after/plugin/transparent.rc.lua
@@ -4,3 +4,8 @@ if (not status) then return end
 transparent.setup({
   enable = true,
 })
+
+-- Disable nvim_transparent on macOS
+if vim.fn.has("macunix") == 1 then
+  vim.api.nvim_command("TransparentDisable")
+end
