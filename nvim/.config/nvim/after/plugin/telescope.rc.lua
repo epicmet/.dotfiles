a096c5930feaafdf908b6a15033a612acfed37be
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-11-08 09:06:09 +0330

    Changed all telescope-file-browser remaps, I want to use `;` more

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -3,6 +3,7 @@ if (not status) then return end
 
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
+local sorters = require('telescope.sorters')
 
 local function telescope_buffer_dir()
   return vim.fn.expand('%:p:h')
@@ -56,37 +57,85 @@ telescope.setup {
 
 telescope.load_extension("file_browser")
 
-vim.keymap.set('n', ';f',
+vim.keymap.set('n', '<leader>gw',
+  function()
+    builtin.grep_string({
+      short_path = true,
+      word_match = "-w",
+      only_sort_text = true,
+      layout_strategy = "vertical",
+      sorter = sorters.get_fzy_sorter()
+    })
+  end)
+vim.keymap.set('n', '<leader>tf',
+  function()
+    builtin.git_files({
+      layout_strategy = "vertical",
+    })
+  end)
+vim.keymap.set('n', '<leader>ta',
   function()
     builtin.find_files({
       no_ignore = false,
+      layout_strategy = "vertical",
       hidden = true
     })
   end)
-vim.keymap.set('n', ';r', function()
-  builtin.live_grep()
-end)
-vim.keymap.set('n', '\\\\', function()
-  builtin.buffers()
-end)
-vim.keymap.set('n', ';t', function()
-  builtin.help_tags()
-end)
-vim.keymap.set('n', ';;', function()
-  builtin.resume()
-end)
-vim.keymap.set('n', ';e', function()
-  builtin.diagnostics()
-end)
-vim.keymap.set("n", "sf", function()
-  telescope.extensions.file_browser.file_browser({
-    path = "%:p:h",
-    cwd = telescope_buffer_dir(),
-    respect_gitignore = false,
-    hidden = true,
-    grouped = true,
-    previewer = false,
-    initial_mode = "normal",
-    layout_config = { height = 40 }
-  })
-end)
+vim.keymap.set('n', '<leader>tr',
+  function()
+    builtin.live_grep()
+  end)
+vim.keymap.set('n', '<leader>tb',
+  function()
+    builtin.buffers()
+  end)
+vim.keymap.set('n', '<leader>th',
+  function()
+    builtin.help_tags()
+  end)
+vim.keymap.set('n', '\\\\',
+  function()
+    builtin.resume()
+  end)
+vim.keymap.set('n', '<leader>te',
+  function()
+    builtin.diagnostics()
+  end)
+vim.keymap.set('n', '<leader>to',
+  function()
+    builtin.oldfiles()
+  end)
+
+
+vim.keymap.set('n', '<leader>gs',
+  function()
+    builtin.git_status({
+      layout_strategy = "vertical",
+    })
+  end)
+vim.keymap.set('n', '<leader>gc',
+  function()
+    builtin.git_commits({
+      layout_strategy = "vertical",
+    })
+  end)
+vim.keymap.set('n', '<leader>gb',
+  function()
+    builtin.git_bcommits({
+      layout_strategy = "vertical",
+    })
+  end)
+
+vim.keymap.set("n", "sf",
+  function()
+    telescope.extensions.file_browser.file_browser({
+      path = "%:p:h",
+      cwd = telescope_buffer_dir(),
+      respect_gitignore = false,
+      hidden = true,
+      grouped = true,
+      previewer = false,
+      initial_mode = "normal",
+      layout_config = { height = 40 }
+    })
+  end)
