c9c0bc6174b0f30bbe77a0ddec777bcb8f45ea83
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-12-16 22:10:02 +0330

    packer bootstrap function

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,7 +1,17 @@
-local status, packer = pcall(require, 'packer')
-if not status then return end
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
 
-vim.cmd([[packadd packer.nvim]])
+local packer_bootstrap = ensure_packer()
+
+local _, packer = pcall(require, 'packer')
 
 packer.startup(function(use)
 	use('wbthomason/packer.nvim')
@@ -58,4 +68,6 @@ packer.startup(function(use)
 	use('rcarriga/nvim-notify')
 
 	use('mbbill/undotree')
+
+	if packer_bootstrap then require('packer').sync() end
 end)
