94133f31598cee6a685484d0ccc15785740205ac
Mahdi Aghaei <mahd.aghaei@gmail.com>
2022-12-16 21:57:14 +0330

    Undo tree. OMG

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,62 +1,61 @@
-local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
-end
+local status, packer = pcall(require, 'packer')
+if not status then return end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use('wbthomason/packer.nvim')
 
-  use 'nvim-lua/plenary.nvim'
+	use('nvim-lua/plenary.nvim')
 
-  use 'folke/tokyonight.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'hoob3rt/lualine.nvim'
-  use 'norcalli/nvim-colorizer.lua'
+	use('folke/tokyonight.nvim')
+	use('kyazdani42/nvim-web-devicons')
+	use('hoob3rt/lualine.nvim')
+	use('norcalli/nvim-colorizer.lua')
 
-  use 'L3MON4D3/LuaSnip'
-  use 'onsails/lspkind.nvim'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  -- use 'glepnir/lspsaga.nvim'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+	use('L3MON4D3/LuaSnip')
+	use('onsails/lspkind.nvim')
+	use('hrsh7th/cmp-path')
+	use('hrsh7th/cmp-buffer')
+	use('hrsh7th/cmp-nvim-lsp')
+	use('hrsh7th/nvim-cmp')
+	use('neovim/nvim-lspconfig')
+	use('williamboman/mason.nvim')
+	use('williamboman/mason-lspconfig.nvim')
+	-- use 'glepnir/lspsaga.nvim'
+	use({
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+	})
 
-  use 'nvim-treesitter/nvim-treesitter-context'
+	use('nvim-treesitter/nvim-treesitter-context')
 
-  use 'windwp/nvim-ts-autotag'
+	use('windwp/nvim-ts-autotag')
 
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'akinsho/nvim-bufferline.lua'
+	use('nvim-telescope/telescope.nvim')
+	use('nvim-telescope/telescope-file-browser.nvim')
+	use('akinsho/nvim-bufferline.lua')
 
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'jay-babu/mason-null-ls.nvim'
-  use 'MunifTanjim/prettier.nvim'
+	use('jose-elias-alvarez/null-ls.nvim')
+	use('jay-babu/mason-null-ls.nvim')
+	use('MunifTanjim/prettier.nvim')
 
-  use 'ThePrimeagen/harpoon'
+	use('ThePrimeagen/harpoon')
 
-  use 'lewis6991/gitsigns.nvim'
+	use('lewis6991/gitsigns.nvim')
 
-  use 'tpope/vim-commentary'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+	use('tpope/vim-commentary')
+	use('JoosepAlviste/nvim-ts-context-commentstring')
 
-  use 'xiyaowong/nvim-transparent'
+	use('xiyaowong/nvim-transparent')
 
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+	use({ 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' })
 
-  use 'ThePrimeagen/vim-be-good'
+	use('ThePrimeagen/vim-be-good')
 
-  use 'simrat39/rust-tools.nvim'
+	use('simrat39/rust-tools.nvim')
 
-  use 'rcarriga/nvim-notify'
+	use('rcarriga/nvim-notify')
+
+	use('mbbill/undotree')
 end)
