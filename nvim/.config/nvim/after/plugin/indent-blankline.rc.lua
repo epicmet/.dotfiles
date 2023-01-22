7ee3efbbf8fed1a21078312ae3bd9a0c3f0966ef
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-01-22 11:04:34 +0330

    (nvim) Made the color of indent-blank line blue

--- nvim/.config/nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.rc.lua
@@ -1,7 +1,12 @@
 local status, indent = pcall(require, 'indent_blankline')
 if not status then return end
 
+vim.cmd([[highlight IndentBlanklineIndent guifg=#61AFEF gui=nocombine]])
+
 indent.setup({
   char = '┊',
   show_trailing_blankline_indent = false,
+  char_highlight_list = {
+    'IndentBlanklineIndent',
+  },
 })
