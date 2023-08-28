4099f60ba47dc64b9aae785650e7d3e1354710ba
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-08-28 12:32:49 +0330

    (nvim) Lsp support for Java

--- nvim/.config/nvim/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/plugin/lspconfig.rc.lua
@@ -100,6 +100,11 @@ nvim_lsp.tsserver.setup({
   capabilities = capabilities,
 })
 
+nvim_lsp.jdtls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
 nvim_lsp.sourcekit.setup({
   on_attach = on_attach,
 })
