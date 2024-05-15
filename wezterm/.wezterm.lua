94366807bccc29102b08ca6910cdd2f2414b4e28
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-05-15 17:56:21 +0330

    (wezterm) Silent not-found icon notif

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.warn_about_missing_glyphs = false
 
 return config
