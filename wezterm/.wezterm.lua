f3e5cb752e3a3d1b07bb0782dd1e7ac4b25927c7
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-09-30 12:28:57 +0330

    (wezterm) Hide tab bar

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
 config.warn_about_missing_glyphs = false
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
