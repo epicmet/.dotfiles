1d1a2867ff5358460b7b9346c26c0388b13a5cb4
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-11-27 20:04:26 +0330

    (nvim) Nordic breaking changes

--- nvim/.config/nvim/after/plugin/nordic.rc.lua
+++ nvim/.config/nvim/after/plugin/nordic.rc.lua
@@ -11,7 +11,7 @@ nordic.setup({
   -- Enable italic comments.
   italic_comments = true,
   -- Enable general editor background transparency.
-  transparent_bg = false,
+  transparent = false,
   -- Enable brighter float border.
   bright_border = false,
   -- Reduce the overall amount of blue in the theme (diverges from base Nord).
@@ -19,7 +19,7 @@ nordic.setup({
   -- Swap the dark background with the normal one.
   swap_backgrounds = false,
   -- Override the styling of any highlight group.
-  override = {},
+  on_highlight = {},
   -- Cursorline options.  Also includes visual/selection.
   cursorline = {
     -- Bold font in cursorline.
