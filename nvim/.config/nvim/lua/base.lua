603512f5cc268193fa8084d4f9a32f364a73ed39
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-12-07 18:39:14 +0330

    (nvim) Couldn't find this damn thing during an interview, so I'm adding a comment!
    I know, skill issue...

--- nvim/.config/nvim/lua/base.lua
+++ nvim/.config/nvim/lua/base.lua
@@ -5,7 +5,7 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
-vim.wo.rnu = true
+vim.wo.rnu = true -- Relative line number
 
 vim.opt.title = true
 vim.opt.hlsearch = false
