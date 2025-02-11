100feb5b8b544cb8273a1d765d381aec8c3e80f4
Mahdi Aghaei <mahd.aghaei@gmail.com>
2025-02-11 11:00:06 +0330

    (nvim) I'm not American

--- nvim/.config/nvim/lua/custom/plugins/gitblame.lua
+++ nvim/.config/nvim/lua/custom/plugins/gitblame.lua
@@ -3,8 +3,8 @@ return {
   'f-person/git-blame.nvim',
   opts = {
     enabled = false, -- Disable by default, Enable with :GitBlameToggle
-    message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
-    date_format = '%m-%d-%Y %H:%M:%S', -- template for the date, check Date format section for more options
-    virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    message_template = ' <summary> • <date> • <author> • <<sha>>',
+    date_format = '%d-%m-%Y %H:%M:%S',
+    virtual_text_column = 1,
   },
 }
